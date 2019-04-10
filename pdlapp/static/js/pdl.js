74706043cdcc65af0760626ae7f3a6afcb5cee32
barskey <barskey@gmail.com>
2019-04-09 20:58:21 -0700

    Adding settings anchor

--- pdlapp/static/js/pdl.js
+++ pdlapp/static/js/pdl.js
@@ -3,11 +3,17 @@ $( document ).ready( function() {
     feather.replace()
   
     $( '.pdl-setting' ).change( function() {
-
+        $( '#status' ).text( 'Saving settings...' );
         $.post('/update_settings', $( '#settingsForm' ).serialize() )
         .done( function( response ) {
+            $( '#status' ).text( response.msg );
             console.log(response.msg);
         });
+    });
+
+    $( 'a.pdl-action' ).click( function(event) {
+        console.log($(event.target));
+        console.log($( event.target ).parent().parent().parent().find( 'select' ).attr('id'));
     });
 
 });
