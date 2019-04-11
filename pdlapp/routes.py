3979f74523320cdfcc5eaa36fb468c2494b4c544
Steve Barskey <steve.barskey@flir.com>
2019-04-10 17:38:47 -0700

    Commit from work

--- pdlapp/routes.py
+++ pdlapp/routes.py
@@ -8,7 +8,8 @@ from pdlapp import app
 @app.route('/index')
 def index():
     settings = json.load(open('pdlapp/settings.json'))
-    return render_template('index.html', settings=settings)
+    rings = json.load(open('pdlapp/rings.json'))
+    return render_template('index.html', settings=settings, rings=rings)
 
 @app.route('/update_settings', methods=['GET', 'POST'])
 def update_settings():
@@ -26,10 +27,44 @@ def update_settings():
         json.dump(settings, outfile)
     return jsonify({'msg': 'Settings saved.'})
 
-def get_status(type):
+@app.route('/get_action_params', methods=['GET', 'POST'])
+def get_action_params():
+    rings = json.load(open('pdlapp/rings.json'))
+    params = rings[request.form.get('ring')][request.form.get('event')]
+    #print (params)
+    return jsonify({'params': params})
+
+@app.route('/update_action', methods=['GET', 'POST'])
+def update_action():
+    rings = json.load(open('pdlapp/rings.json'))
+    rings[request.form.get('ring')][request.form.get('event')] = {
+            'action': request.form.get('action'),
+            'color1': request.form.get('color1', 'rgb(0, 0, 0)'),
+            'color2': request.form.get('color2','rgb(0, 0, 0)'),
+            'interval': request.form.get('interval', '')
+        }
+    with open('pdlapp/rings.json', 'w') as outfile:
+        json.dump(rings, outfile)
+    return jsonify({'msg': 'Action saved.'})
+
+def get_duet_status(type):
     settings = json.load(open('pdlapp/settings.json'))
     host = 'http://' + settings['hostname'] + '/rr_status'
     args = {'type': type}
     result = requests.post(host, json=args)
     print (result.text)
     return result.json()
+
+def solid_color(color, order):
+    # color is rgb(#, #, #)
+    rgb = color[4:-1].split(',')
+    r = rgb[0].strip()
+    g = rgb[1].strip()
+    b = rgb[2].strip()
+    if order == "RGB":
+        #pixels.fill((r,g,b))
+        pass
+    else:
+        #pixels.fill((g,r,b))
+        pass
+    #pixels.show()
--- pdlapp/static/js/pdl.js
+++ pdlapp/static/js/pdl.js
@@ -1,6 +1,32 @@
 $( document ).ready( function() {
   
     feather.replace()
+
+    var actionDesc = [
+        ['', '', ''],
+        ['', '', ''],
+        ['Ring shows selected color.', 'Color:', 'Not Used'],
+        ['Ring will show percent complete as hotend temperature reaches target.', '% Complete:', 'Background:'],
+        ['Ring will show percent complete as heatbed temperature reaches target.', '% Complete:', 'Background:'],
+        ['Ring changes between two colors switching at specified interval.', 'Color 1:', 'Color 2:'],
+        ['Ring fades gradually between two colors over specified interval.', 'Color 1:', 'Color 2:'],
+        ['Chase color spins around ring completing one rotation in specified interval.', 'Chase:', 'Background:']
+    ]
+
+    $( '#color1, #color2' ).spectrum({
+        showPaletteOnly: true,
+        togglePaletteOnly: true,
+        togglePaleteMoreText: 'more',
+        togglePaleteLessText: 'less',
+        hideAfterPaletteSelect: true,
+        preferredFormat: 'rgb',
+        color: '#000000',
+        palette: [
+            ['#fff', '#f00', '#f90', '#ff0'],
+            ['#888', '#0f0', '#090', '#0ff'],
+            ['#000', '#00f', '#90f', '#f0f']
+        ]
+    });
   
     $( '.pdl-setting' ).change( function() {
         $( '#status' ).text( 'Saving settings...' );
@@ -11,9 +37,76 @@ $( document ).ready( function() {
         });
     });
 
-    $( 'a.pdl-action' ).click( function(event) {
-        console.log($(event.target));
-        console.log($( event.target ).parent().parent().parent().find( 'select' ).attr('id'));
+    function showActionModal( action, ring, evnt, params ) {
+        var $modal = $( '#paramModal' );
+        $modal.find( '#interval' ).removeClass( 'is-invalid' );
+        $modal.find( '#action-desc' ).text( actionDesc[action][0] );
+        $modal.find( '#action-color1label' ).text( actionDesc[action][1] );
+        $modal.find( '#action-color2label' ).text( actionDesc[action][2] );
+        $modal.find( '#ring' ).val( ring );
+        $modal.find( '#event' ).val( evnt );
+        $modal.find( '#action' ).val( action );
+
+        console.log(action < 5);
+        if( action < 5 ) { // disable interval for these
+            $modal.find( '#interval' ).val( '' );
+            $modal.find( '#interval' ).attr( 'readonly', true );
+        } else {
+            $modal.find( '#interval' ).attr( 'readonly', false );
+        }
+        if ( action == 2 ) { // disable second color for 2-solid
+            $modal.find( '#color2' ).spectrum( 'disable' );
+        } else {
+            $modal.find( '#color2' ).spectrum( 'enable' );
+        }
+
+        $modal.find( '#color1' ).spectrum( 'set', params.color1 );
+        $modal.find( '#color2' ).spectrum( 'set', params.color2 );
+        $modal.find( '#interval' ).val( params.interval );
+        $modal.modal( 'show' );
+    }
+
+    $( 'a.pdl-action' ).click( function() {
+        var action = $( this ).parent().parent().find( 'select option:selected' ).val();
+        var ring = $( this ).attr( 'id' ).split( '-' )[1];
+        var evnt = $( this ).attr( 'id' ).split( '-' )[2];
+        //console.log(action, ring, evnt);
+        if ( action > 1 ) {
+            $.post( '/get_action_params', {'ring': ring, 'event': evnt} )
+            .done( function( response ) {
+                showActionModal( action, ring, evnt, response.params );
+            })
+            .fail( function (xhr, status, err) {
+                $( '#status' ).text( err )
+            });
+        }
+    });
+
+    $( 'select.pdl-action' ).change( function() {
+        var action = $( this ).children( 'option:selected' ).val();
+        var ring = $( this ).attr( 'id' ).split( '-' )[1];
+        var evnt = $( this ).attr( 'id' ).split( '-' )[2];
+        //console.log(action, ring, evnt);
+        if ( action > 1 ) {
+            $.post( '/get_action_params', {'ring': ring, 'event': evnt} )
+            .done( function( response ) {
+                showActionModal( action, ring, evnt, response.params );
+            });
+        }
+    });
+
+    $( '#modalDone' ).click( function() {
+        var $modal = $( '#paramModal' );
+        if( ['5','6','7'].includes( $modal.find( '#action' ).val() ) && $modal.find( '#interval' ).val() == '' ) {
+            $modal.find( '#interval' ).addClass( 'is-invalid' );
+            return false;
+        }
+        $.post( '/update_action', $modal.find('form').serialize() )
+        .done( function( response ) {
+            $( '#status' ).text( response.msg );
+            console.log(response.msg);
+        });
+        $modal.modal( 'hide' );
     });
 
 });
