4da4278f56500a2a99280b30710014de0a72cb09
Steve Barskey <>
2019-08-12 19:13:57 -0700

    Fix to complete state logic

--- app/printer_status.py
+++ app/printer_status.py
@@ -84,10 +84,17 @@ class PrinterStatus:
     def state(self, value):
         if value != self._state:
             logger.debug('<-set state-> Printer state change detected!')
-            self.needs_update = True
-            self._prev_state = self._state
             # set to 'complete' state if changing to Idle from Print, else set to new state
-            self._state = 'Z' if self._prev_state == 'P' and value == 'I' else value
+            if value == 'I': # if changing to idle
+                if self._state == 'P': # if changing from P
+                    self._state = 'Z' # set to complete state instead of I
+                    self.needs_update = True
+                elif self._state != 'Z': # but don't change to idle if print is complete
+                    self._state = value
+                    self.needs_update = True
+            else: # otherwise change to new state
+                self._state = value
+                self.needs_update = True
 
     def get_task(self, ringnum):
         return self._tasks[ringnum]
